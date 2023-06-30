03446b60240fce2876ddba53fd3f49b9de12d4da
adityamer33 <128570856+adityamer33@users.noreply.github.com>
2023-07-01 03:27:18 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,6 +1,6 @@
 # enter your API credentials below - generate at https://my.telegram.org/auth
-api_id = 28867937
-api_hash = "6fae6638ccadf4140c40ae4693962465"
+api_id =12345678
+api_hash = "6a6355898ccadf4140c455668965489"
 
 #THIS IS JUST A GENERIC API_ID AND API_HASH!
 
